d55a907e765d8af0c7e1a8a52cae477260c0b4f1
Yemi Bedu @ P&R <yemi@weldfast.com>
2017-04-06 13:04:14 -0400

    Grammar update
    update to grammar

--- JavaScript/Pro JavaScript Techniques/Chapters/02/code/11-global.js
+++ JavaScript/Pro JavaScript Techniques/Chapters/02/code/11-global.js
@@ -1,7 +1,7 @@
 // A globally-scope variable, containing the string 'test'
 var test = "test";
 
-// You'll notice that our 'global' variable and the the
-// property of the the window object are identical
+// You'll notice that our 'global' variable and the
+// property of the window object are identical
 alert( window.test == test );
 
